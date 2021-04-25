f08d7ba8bfffda579fcd611f9acb7ba2d0055efc
unknown <yellow72693@yahoo.com>
2021-04-25 16:05:09 -0400

    added landing page and new files

---
+++ main.js
@@ -0,0 +1,16 @@
+function mobileMenu() {
+  var node = document.getElementById("nav-middle-links");
+  document.getElementById("menu-display").appendChild(node);
+  toggle();
+}
+
+function toggle() {
+  var x = document.getElementById("menu-display");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  else {
+    x.style.display = "block";
+  }
+}
+document.getElementById("nav-middle-links").addEventListener("click", mobileMenu);
