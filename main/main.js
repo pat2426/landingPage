5181d4e1a9c03ba3efc36e2a224689eed59a3ac3
unknown <yellow72693@yahoo.com>
2021-06-08 21:19:31 -0400

    working with the hamburger menu for mobile view

--- main/main.js
+++ main/main.js
@@ -1,13 +1,13 @@
 /*need to continue to work on this*/
 function mobileMenu() {
-  var node = document.getElementById("nav-middle-links");
+  var node = document.getElementById("middle_menu");
   document.getElementById("menu-display").appendChild(node);
   toggle();
 }
 
 function toggle() {
   var x = document.getElementById("menu-display");
-  if (x.style.display === "block") {
+  if (x.style.display = "block") {
     x.style.display = "none";
   }
   else {
