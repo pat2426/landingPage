a5cbfdce47783de5c44b28eaf3655acd0cf7f396
unknown <yellow72693@yahoo.com>
2021-06-13 22:19:36 -0400

    working with the hamburger menu

--- main/main.js
+++ main/main.js
@@ -9,7 +9,7 @@ function mobileMenu() {
 function toggle() {
   var x = document.getElementById("menu-display");
   if (x.style.display = "block") {
-    x.style.display = "none";
+    x.style.display = "inline-block";
   }
   else {
     x.style.display = "block";
