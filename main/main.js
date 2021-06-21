313ac39a1d573abd1fbea2ac79fbfb02a2edb1cc
unknown <yellow72693@yahoo.com>
2021-06-21 09:33:57 -0400

    working with js

--- main/main.js
+++ main/main.js
@@ -8,7 +8,7 @@ function mobileMenu() {
 /*Toggle function*/
 function toggle() {
   var x = document.getElementById("menu-display");
-  if (x.style.display = "block") {
+  if (x.style.display = "inline-block") {
     x.style.display = "left";
   }
   else {
