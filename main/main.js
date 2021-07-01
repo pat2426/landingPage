0e222cb96d750248f39d8d1ab50527e80f7266f9
unknown <yellow72693@yahoo.com>
2021-07-01 18:55:49 -0400

    Switched around hamburger menu

--- main/main.js
+++ main/main.js
@@ -8,8 +8,8 @@ function mobileMenu() {
 /*Toggle function*/
 function toggle() {
   var x = document.getElementById("menu-display");
-  if (x.style.display = "inline-block") {
-    x.style.display = "inline-block";
+  if (x.style.display = "block") {
+    x.style.display = "none";
   }
   else {
     x.style.display = "inline-block";
