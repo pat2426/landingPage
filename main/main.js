5bc9649f7dc77e2b90368af482a06b346b1f71dc
unknown <yellow72693@yahoo.com>
2021-07-28 18:15:25 -0400

    working on js file

--- main/main.js
+++ main/main.js
@@ -9,7 +9,7 @@ function mobileMenu() {
 function toggle() {
   var x = document.getElementById("menu-display");
   if (x.style.display = "block") {
-    x.style.display = "none";
+    x.style.display = "left";
   }
   else {
     x.style.display = "block";
